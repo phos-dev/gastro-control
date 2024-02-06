3ab94905a3bae87388c297e383b30aebb5f090eb
augustocsmiranda <augustocezarmiranda@gmail.com>
2024-02-06 12:43:57 -0300

    Create QRCoderController.cs
    QRCoder Controller Criado.

---
+++ services/core/Controllers/QRCoderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QRCodeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Generate(string content)
+        {
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
+                using (QRCode qrCode = new QRCode(qrCodeData))
+                {
+                    using (Bitmap bitmap = qrCode.GetGraphic(20))
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                            ms.Position = 0;
+                            return File(ms.ToArray(), "image/png");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
